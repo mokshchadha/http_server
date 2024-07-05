0463e014c06c528c080a30ebe5e9840093043891
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-07-05 23:43:42 +0530

    added response.send function along with bad request handling properly

--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,6 @@
 use std::fmt::{Display, Formatter};
+use std::io::{Write, Result};
+use std::net::TcpStream;
 
 #[derive(Copy, Clone)]
 pub enum StatusCode {
@@ -29,11 +31,20 @@ pub struct Response {
 }
 
 impl Response {
-   pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Self {
             status_code,
             body,
         }
+    }
+
+
+    pub fn send(&self, stream: &mut TcpStream) -> Result<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => ""
+        };
+        write!(stream, "HTTP/1.1 {} {}\r\n\r\n{}", self.status_code, self.status_code.reason_phrase(), body)
     }
 }
 
--- src/server.rs
+++ src/server.rs
@@ -17,27 +17,31 @@ impl Server {
     }
 
     pub fn run(&self) {
-        println!("inside run function my niga");
         let listener = TcpListener::bind(&self.addr).unwrap();
 
         loop {
             match listener.accept() {
                 Ok((mut stream, addr)) => {
-                    let mut buffer= [0; 1024]; // there is a chance of breakage
-                   match stream.read(&mut buffer) {
-                       Ok(_) => {
-                           println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                           match Request::try_from(&buffer[..]) {
-                               Ok(req) => {
-                                   let resp = Response::new(StatusCode::Ok, None);
-                                   write!(stream, "{}", resp);
-                               },
-                               Err(e) => println!("Failed to pass an request {}", e)
-                           }
+                    let mut buffer = [0; 1024]; // there is a chance of breakage
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(req) => {
+                                    Response::new(StatusCode::Ok, Some("<h1> Hello world </h1>".to_string()))
+                                }
+                                Err(e) => {
+                                    println!("Failed to pass an request {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
 
-                       },
-                       Err(e) => println!("Failed to read from connection {}", e)
-                   }
+                            if let Err(e) = response.send(&mut stream  ) {
+                                println!("Failed to send response")
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection {}", e)
+                    }
                 }
                 Err(e) => {
                     println!("Error failed to establish connection {}", e);
