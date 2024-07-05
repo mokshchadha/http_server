5ce7a9a1808270dfcdbe8d24c3eddd5e17ec42c6
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-07-05 17:38:45 +0530

    request parsing complete

--- src/request.rs
+++ src/request.rs
@@ -1,4 +1,8 @@
-use crate::method::Method;
+use crate::method::{Method, MethodError};
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::str::{self, Utf8Error};
 
 
 pub struct Request {
@@ -6,3 +10,83 @@ pub struct Request {
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    fn try_from(buff: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buff)?;
+        let (method, remaining) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, remaining) = get_next_word(remaining).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(remaining).ok_or(ParseError::InvalidRequest)?;
+
+        if (protocol != "HTTP/1.1") {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(path[i + 1..].to_string());
+            path = &path[..i];
+        }
+
+        Ok(Self{
+            path : path.to_string(), query_string, method
+        })
+    }
+}
+
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidRequest",
+            Self::InvalidProtocol => "InvalidRequest",
+            Self::InvalidMethod => "InvalidRequest",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use std::io::Read;
 use std::net::TcpListener;
+use crate::request::Request;
+
 
 pub struct Server {
     addr: String,
@@ -17,7 +20,26 @@ impl Server {
         let listener = TcpListener::bind(&self.addr).unwrap();
 
         loop {
-            listener.accept();
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer= [0; 1024]; // there is a chance of breakage
+                   match stream.read(&mut buffer) {
+                       Ok(_) => {
+                           println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                           match Request::try_from(&buffer[..]) {
+                               Ok(req) => {},
+                               Err(e) => println!("Failed to pass an request {}", e)
+                           }
+
+                       },
+                       Err(e) => println!("Failed to read from connection {}", e)
+                   }
+                }
+                Err(e) => {
+                    println!("Error failed to establish connection {}", e);
+                    continue;
+                }
+            }
         }
     }
 }
